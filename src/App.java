dbcec61d0722698ef44fdfb78b144b5a51bd9818
Anne Le <anne.h.s.le@gmail.com>
2025-09-30 13:38:28 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+            String tekija = "Anne";
+  
+double luku1 = 5;
+double luku2 = 2;
+
+double tulo = luku1*luku2;
+double summa = luku1+luku2;
+double erotus = luku1-luku2;
+double jako = luku1/luku2;
+  
+    System.out.println("Hei olen Tulostin-ohjelma");
+    System.out.println("Ohjelman tekijä: "+ tekija);
+
+System.out.println("Luku1-muuttujan arvo on " + luku1);
+System.out.println("Luku2-muuttujan arvo on " + luku2);
+
+System.out.println(luku1 + " + " + luku2 +" = "+ summa);
+System.out.println(luku1 + " - " + luku2 +" = "+ erotus);
+System.out.println(luku1 + " * " + luku2 +" = "+ tulo);
+System.out.println(luku1 + " / " + luku2 +" = "+ jako);
+    }
+}
